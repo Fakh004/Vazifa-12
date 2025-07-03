48c270fee1388f56053556b9471baa615b09f2aa
fakh <fakhridin008@gmail.com>
2025-07-03 17:45:20 +0300

    Vaz

---
+++ HomeWork.py
@@ -0,0 +1,50 @@
+
+# n= input('Jumla--> ')
+# with open("file.txt", "w") as file:
+#     a=file.write(n)
+
+# -------------------------------------------
+
+# with open('file.txt','r') as myfile:
+#     a=myfile.read()
+#     print(a)
+
+# -------------------------------------------
+
+# with open('file.txt','r') as file:
+#     a=file.readlines()
+#     for i in range(len(a)):
+#         print(f'{i+1}: {a[i]}')
+
+# -------------------------------------------
+
+# with open('file.txt','r') as file:
+#     b=file.read()
+#     print(len(b))
+
+# -------------------------------------------
+
+# with open('file.txt', 'r') as file:
+#     text=file.read()
+#     lst=text.split()
+#     print(len(lst))
+
+# -------------------------------------------
+
+# poisk=input('poisk --> ')
+# with open('file.txt','r') as myfile:
+#     txt=myfile.readlines()
+#nashid
+# -------------------------------------------
+
+# a=input('Jumla--> ')
+# with open('file.txt','a') as myfile:
+#     b=myfile.write(a)
+
+# -------------------------------------------
+
+# with open('file.txt', 'r') as myfile:
+#     a=myfile.read()
+# with open('text7_copy.txt', 'w') as myfile2:
+#     b=myfile2.write(a)
+
